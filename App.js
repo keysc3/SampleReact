827c1cdae920fbd891d9a81b5a1d5b12a5b8d3ed
rehmana66 <rehmana6@mymacewan.ca>
2019-07-16 17:51:37 -0600

    removed header and working bottomnav bar

--- App.js
+++ App.js
@@ -7,9 +7,10 @@
  */
 
 
-import React, { Component } from 'react';
+import React, { Component,  } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer  } from 'react-navigation';
+import { ToolbarAndroid } from 'react-native-gesture-handler';
 import Login from './src/components/Login/';
 import Home from './src/components/Home';
 import Dashboard from './src/components/Dashboard/';
@@ -17,18 +18,33 @@ import Jobs from './src/components/Dashboard/jobs.js';
 import Register from './src/components/Register';
 
 const AppNavigator = createStackNavigator({
-  home: Home,
+  home: {
+    screen: Home,
+    navigationOptions: {
+    header: null
+    }
+  },
   login: Login,
   register: Register,
   dash: Dashboard,
-  jobs: Jobs
-});
+  jobs: Jobs,
+  },
+
+
+);
 
 //const AppContainer = createAppContainer(RootStack);
 const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends Component {
+  static navigationOptions = {
+    header: null
+}
   render() {
-    return <AppContainer />;
+    return (
+     
+    <AppContainer />
+    
+    );
   }
 }
