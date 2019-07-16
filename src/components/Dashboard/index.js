06ab58ec165511f69d4b9af896f73e849b10e4b3
rehmana66 <rehmana6@mymacewan.ca>
2019-07-15 22:45:08 -0600

    bottom navigation icon change

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, Button, TouchableOpacity, TouchableHighlight } from 'react-native';
+import { View, Text, Container, StyleSheet, Image, Button, TouchableOpacity, TouchableHighlight } from 'react-native';
+
+
+
 
 class Dashboard extends Component {
+    
     constructor(props) {
         super(props)
         this.state = { uri: 'https://img.icons8.com/ios/64/000000/home.png' }
       }
+
     render() {
+        
+
         return (
+            
             <View style={styles.mainContainer}>
+                
                 <View style = {styles.top}>
                     <Text style={styles.label}>Top</Text>
                 </View>
@@ -21,28 +30,28 @@ class Dashboard extends Component {
                         <View style={styles.bottomnav}>
                             <TouchableOpacity onPress={() => this.props.navigation.navigate('dash')}>
                                 <Image style={styles.iconsize}
-                                source={{uri: 'https://img.icons8.com/ios/64/000000/home.png'}}/>
+                                source={{uri: 'https://img.icons8.com/wired/64/000000/chat.png'}}/>
                             </TouchableOpacity>
                             <Text style={styles.label}>Home</Text>
                         </View>
                             <View style={styles.bottomnav}> 
                             <TouchableOpacity onPress = {() => this.props.navigation.navigate('dash')}>
                                 <Image style={styles.iconsize}
-                                source={{uri: 'https://img.icons8.com/ios/64/000000/search--v1.png'}}/>
+                                source={{uri: 'https://img.icons8.com/wired/64/000000/search.png'}}/>
                             </TouchableOpacity>
                             <Text style={styles.label}>Jobs</Text>
                         </View>
                             <View style={styles.bottomnav}> 
                             <TouchableOpacity onPress = {() => this.props.navigation.navigate('dash')}>
                                 <Image style={styles.iconsize}
-                                source={{uri: 'https://img.icons8.com/ios/64/000000/groups.png'}}/>
+                                source={{uri: 'https://img.icons8.com/dotty/80/000000/group-foreground-selected.png'}}/>
                             </TouchableOpacity>
                             <Text style={styles.label}>Groups</Text>
                         </View>
                         <View style={styles.bottomnav}> 
                             <TouchableOpacity onPress = {() => this.props.navigation.navigate('dash')}>
                                 <Image style={styles.iconsize}
-                                source={{uri: 'https://img.icons8.com/ios/64/000000/settings.png'}}/>
+                                source={{uri: 'https://img.icons8.com/wired/64/000000/gear.png'}}/>
                             </TouchableOpacity>
                             <Text style={styles.label}>Settings</Text>
                         </View>
@@ -63,24 +72,28 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     middle: {
-        flex: 7,
+        flex: 8,
     },
     
     bottom: {
-       flex: 1,
-       
+        flex: 1,
+        backgroundColor: "#E8E8E8",
+
     },
     topnav: {
-        paddingTop: 10,
+        paddingTop: 8,
         flexDirection: "row",
         justifyContent: "space-evenly",
         paddingBottom: 0,
+        
     },
     bottomnav: {
         flex: 1,
         flexDirection: 'column',
         alignItems: 'center',
         paddingLeft: 10,
+        
+        
     },
     iconsize: {
         height: 30,
@@ -89,7 +102,11 @@ const styles = StyleSheet.create({
     dividerline: {
         borderBottomColor: 'black',
         borderBottomWidth: 1,
+        
+        
+        
     },
+
 });
 
 export default Dashboard;
